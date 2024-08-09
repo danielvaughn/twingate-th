0a7d3a85f6cc5a596e9d1ce4134e516a8a0b741b
Daniel Vaughn <danielbvaughn@gmail.com>
2024-08-09 16:23:25 -0400

    adds final touches

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -3,8 +3,11 @@ import { DataNode } from '../types'
 
 export default function Data({ datum }: { datum: DataNode }) {
   const [datumText, setDataText] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
   const fetchData = async (url: string) => {
+    setIsLoading(true)
+
     const response = await fetch(url)
 
     if (response.status < 299) {
@@ -13,6 +16,8 @@ export default function Data({ datum }: { datum: DataNode }) {
         setDataText(rawData.substring(0, 500))
       }
     }
+
+    setIsLoading(false)
   }
 
   useEffect(() => {
@@ -20,13 +25,31 @@ export default function Data({ datum }: { datum: DataNode }) {
   }, [datum.url])
 
   return (
-    <div className="p-5 border-t border-neutral-800 mt-5">
-      {datumText === '' && (
-        <span>nothing to show yet!</span>
+    <div className="p-5 border-t border-neutral-800 mt-5 flex gap-5">
+      <div>
+        <button
+          type="button"
+          className="px-2 py-1 bg-blue-500 text-white hover:cursor hover:bg-blue-800"
+          onClick={() => {
+            fetchData(datum.url)
+          }}
+        >
+          refresh
+        </button>
+      </div>
+      {isLoading && (
+        <div>loading your data!</div>
       )}
+      {!isLoading && (
+        <div>
+          {datumText === '' && (
+            <span>nothing to show!</span>
+          )}
 
-      {datumText !== '' && (
-        <span>{datumText}</span>
+          {datumText !== '' && (
+            <span>{datumText}</span>
+          )}
+        </div>
       )}
     </div>
   )
